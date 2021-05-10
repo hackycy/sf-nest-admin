1eaabd38d41ce94967e83bc4a7f5934bbc2a8390
hackycy <qa894178522@qq.com>
2021-05-10 18:28:44 +0800

    feat：inject REDIS_INSTANCE

--- src/admin/login/login.service.ts
+++ src/admin/login/login.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Redis } from 'ioredis';
+import { REDIS_INSTANCE } from 'src/common/contants/common.contants';
 
 @Injectable()
 export class AdminLoginService {
-  private redis: Redis;
+  constructor(@Inject(REDIS_INSTANCE) private redis: Redis) {}
 
   async getRedisPasswordVersionById(id: number): Promise<string> {
     return this.redis.get(`admin:passwordVersion:${id}`);
