76dea041e609d20d9ea647a163d7a3c11d153928
hackycy <qa894178522@qq.com>
2021-05-11 12:28:16 +0800

    feat：add support swagger

--- src/modules/admin/login/login.controller.ts
+++ src/modules/admin/login/login.controller.ts
@@ -1,14 +1,19 @@
 import { Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AdminController } from '../core/decorators/admin-controller.decorator';
 import { Open } from '../core/decorators/open.decorator';
 import { ImageCaptchaDto } from './login.dto';
 import { IImageCaptcha } from './login.interface';
 import { AdminLoginService } from './login.service';
 
+@ApiTags('登录模块')
 @AdminController('public')
 export class AdminLoginController {
   constructor(private loginService: AdminLoginService) {}
 
+  @ApiOperation({
+    summary: '获取登录图片验证码',
+  })
   @Get('captcha/img')
   @Open()
   async get(@Query() dto: ImageCaptchaDto): Promise<IImageCaptcha> {
--- src/modules/admin/login/login.dto.ts
+++ src/modules/admin/login/login.dto.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumberString, IsOptional } from 'class-validator';
 
 export class ImageCaptchaDto {
+  @ApiProperty({
+    required: false,
+    default: 100,
+    description: '验证码宽度',
+  })
   @IsNumberString()
   @IsOptional()
   width: number;
 
+  @ApiProperty({
+    required: false,
+    default: 50,
+    description: '验证码宽度',
+  })
   @IsNumberString()
   @IsOptional()
   height: number;
