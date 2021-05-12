d0f50737fc094b53c8441ad6a1807bdf9e2b9103
hackycy <qa894178522@qq.com>
2021-05-12 16:01:36 +0800

    styles：add swagger default value

--- src/common/dto/page.dto.ts
+++ src/common/dto/page.dto.ts
@@ -5,6 +5,8 @@ import { IsInt, Min } from 'class-validator';
 export class PageOptionsDto {
   @ApiProperty({
     description: '当前页包含数量',
+    required: false,
+    default: 10,
   })
   @Type(() => Number)
   @IsInt()
@@ -13,6 +15,8 @@ export class PageOptionsDto {
 
   @ApiProperty({
     description: '当前页包含数量',
+    required: false,
+    default: 1,
   })
   @Type(() => Number)
   @IsInt()
