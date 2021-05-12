4618acc7690d8fcd3ee7761ec4fa25c92c57f3d6
hackycy <qa894178522@qq.com>
2021-05-12 15:25:35 +0800

    feat：migrate dept module

---
+++ src/modules/admin/system/dept/dept.dto.ts
@@ -0,0 +1,80 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+  MinLength,
+  ValidateIf,
+  ValidateNested,
+} from 'class-validator';
+
+export class CreateDeptDto {
+  @ApiProperty({ description: '部门名称' })
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @ApiProperty({ description: '父级部门id' })
+  @IsInt()
+  parentId: number;
+
+  @ApiProperty({ description: '排序编号', required: false })
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  orderNum: number;
+}
+
+export class UpdateDeptDto extends CreateDeptDto {
+  @ApiProperty({ description: '需要更新的部门id' })
+  @IsInt()
+  @Min(0)
+  id: number;
+}
+
+export class DeleteDeptDto {
+  @ApiProperty({ description: '删除的系统部门ID' })
+  @IsInt()
+  @Min(0)
+  departmentId: number;
+}
+
+export class InfoDeptDto {
+  @ApiProperty({ description: '查询的系统部门ID' })
+  @IsInt()
+  @Min(0)
+  departmentId: number;
+}
+
+export class TransferDeptDto {
+  @ApiProperty({ description: '需要转移的管理员列表编号', type: [Number] })
+  @IsArray()
+  @ArrayNotEmpty()
+  userIds: number[];
+
+  @ApiProperty({ description: '需要转移过去的系统部门ID' })
+  @IsInt()
+  @Min(0)
+  departmentId: number;
+}
+
+export class MoveDeptDto {
+  @ValidateNested()
+  depts: MoveDept[];
+}
+
+export class MoveDept {
+  @ApiProperty({ description: '当前部门ID' })
+  @IsInt()
+  @Min(0)
+  id: number;
+
+  @ApiProperty({ description: '移动到指定父级部门的ID' })
+  @IsInt()
+  @Min(0)
+  @ValidateIf((o) => o.parentId === null || o.parentId === undefined)
+  parentId?: number;
+}
--- src/modules/admin/system/system.module.ts
+++ src/modules/admin/system/system.module.ts
@@ -7,6 +7,8 @@ import SysRoleMenu from 'src/entities/admin/sys-role-menu.entity';
 import SysRole from 'src/entities/admin/sys-role.entity';
 import SysUserRole from 'src/entities/admin/sys-user-role.entity';
 import SysUser from 'src/entities/admin/sys-user.entity';
+import { SysDeptController } from './dept/dept.controller';
+import { SysDeptService } from './dept/dept.service';
 import { SysMenuController } from './menu/menu.controller';
 import { SysMenuService } from './menu/menu.service';
 import { SysRoleController } from './role/role.controller';
@@ -27,8 +29,13 @@ import { SysUserService } from './user/user.service';
       SysUserRole,
     ]),
   ],
-  controllers: [SysUserController, SysRoleController, SysMenuController],
-  providers: [SysUserService, SysRoleService, SysMenuService],
-  exports: [SysUserService, SysRoleService, SysMenuService],
+  controllers: [
+    SysUserController,
+    SysRoleController,
+    SysMenuController,
+    SysDeptController,
+  ],
+  providers: [SysUserService, SysRoleService, SysMenuService, SysDeptService],
+  exports: [SysUserService, SysMenuService],
 })
 export class SystemModule {}
