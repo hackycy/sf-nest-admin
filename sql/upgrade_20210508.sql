adb2588e5c715d4d5095f6c96d0a145945b54e2e
小小源逼 <qa894178522@qq.com>
2021-05-16 01:45:33 +0800

    chore：backup sql

--- sql/upgrade_20210508.sql
+++ sql/upgrade_20210508.sql
@@ -4,10 +4,16 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-INSERT INTO `sf-admin`.sys_menu (createTime, updateTime, id, parent_id, name, router, perms, `type`, icon, order_num, view_path, keepalive, isShow) VALUES('2021-05-08 03:06:41.536985000', '2021-05-08 03:06:41.536985000', 69, NULL, '文件空间', '/file/space', NULL, 1, 'space', 255, 'views/file/space', 1, 1);
-INSERT INTO `sf-admin`.sys_menu (createTime, updateTime, id, parent_id, name, router, perms, `type`, icon, order_num, view_path, keepalive, isShow) VALUES('2021-05-08 03:07:00.710454000', '2021-05-08 03:07:00.710454000', 70, 69, '查询', NULL, 'file:space:list', 2, NULL, 255, NULL, 1, 1);
-INSERT INTO `sf-admin`.sys_menu (createTime, updateTime, id, parent_id, name, router, perms, `type`, icon, order_num, view_path, keepalive, isShow) VALUES('2021-05-08 03:07:36.804612000', '2021-05-08 03:07:44', 71, 69, '删除', NULL, 'file:space:delete,file:space:check', 2, NULL, 255, NULL, 1, 1);
-INSERT INTO `sf-admin`.sys_menu (createTime, updateTime, id, parent_id, name, router, perms, `type`, icon, order_num, view_path, keepalive, isShow) VALUES('2021-05-08 03:08:16.445447000', '2021-05-08 03:08:16.445447000', 72, 69, '创建文件夹', NULL, 'file:space:mkdir', 2, NULL, 255, NULL, 1, 1);
-INSERT INTO `sf-admin`.sys_menu (createTime, updateTime, id, parent_id, name, router, perms, `type`, icon, order_num, view_path, keepalive, isShow) VALUES('2021-05-08 03:08:34.173745000', '2021-05-08 03:08:34.173745000', 73, 69, '上传', NULL, 'file:space:token', 2, NULL, 255, NULL, 1, 1);
-INSERT INTO `sf-admin`.sys_menu (createTime, updateTime, id, parent_id, name, router, perms, `type`, icon, order_num, view_path, keepalive, isShow) VALUES('2021-05-08 03:08:55.860051000', '2021-05-08 03:09:27', 74, 69, '重命名', NULL, 'file:space:rename,file:space:check', 2, NULL, 255, NULL, 1, 1);
-INSERT INTO `sf-admin`.sys_menu (createTime, updateTime, id, parent_id, name, router, perms, `type`, icon, order_num, view_path, keepalive, isShow) VALUES('2021-05-08 03:09:19.551524000', '2021-05-08 03:09:19.551524000', 75, 69, '下载', NULL, 'file:space:download', 2, NULL, 255, NULL, 1, 1);
+--
+-- 转存表中的数据 `sys_menu`
+--
+INSERT INTO `sys_menu` (`createTime`, `updateTime`, `id`, `parent_id`, `name`, `router`, `perms`, `type`, `icon`, `order_num`, `view_path`, `keepalive`, `isShow`) VALUES
+('2021-05-16 01:38:40.990691', '2021-05-16 01:38:40.990691', 72, NULL, '网盘空间', '/netdisk', NULL, 0, 'netdisk', 255, NULL, 1, 1),
+('2021-05-16 01:39:06.265986', '2021-05-16 01:39:06.265986', 73, 72, '空间管理', '/netdisk/manage', NULL, 1, 'netdisk-manage', 255, 'views/netdisk/manage', 1, 1),
+('2021-05-16 01:40:03.423681', '2021-05-16 01:40:03.423681', 74, 73, '查询', NULL, 'netdisk:manage:list', 2, NULL, 255, NULL, 1, 1),
+('2021-05-16 01:40:27.605473', '2021-05-16 01:40:27.605473', 75, 73, '创建文件夹', NULL, 'netdisk:manage:mkdir', 2, NULL, 255, NULL, 1, 1),
+('2021-05-16 01:40:42.986572', '2021-05-16 01:40:42.986572', 76, 73, '上传', NULL, 'netdisk:manage:token', 2, NULL, 255, NULL, 1, 1),
+('2021-05-16 01:40:57.687251', '2021-05-16 01:41:36.000000', 77, 73, '重命名', NULL, 'netdisk:manage:rename,netdisk:manage:check', 2, NULL, 255, NULL, 1, 1),
+('2021-05-16 01:41:15.070191', '2021-05-16 01:41:15.070191', 78, 73, '下载', NULL, 'netdisk:manage:download', 2, NULL, 255, NULL, 1, 1),
+('2021-05-16 01:41:56.637858', '2021-05-16 01:41:56.637858', 79, 73, '删除', NULL, 'netdisk:manage:delete,netdisk:manage:check', 2, NULL, 255, NULL, 1, 1),
+('2021-05-16 01:42:17.793185', '2021-05-16 01:42:17.793185', 80, 73, '预览', NULL, 'netdisk:manage:info', 2, NULL, 255, NULL, 1, 1);
