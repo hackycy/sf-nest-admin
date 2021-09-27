26cadc8c638e41084a350a8cade0cf00c6a8f840
hackycy <qa894178522@qq.com>
2021-09-27 17:57:25 +0800

    feat: impl param config page api

--- src/modules/admin/system/param-config/param-config.controller.ts
+++ src/modules/admin/system/param-config/param-config.controller.ts
@@ -1,6 +1,31 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PageResult } from 'src/common/class/res.class';
+import { PageOptionsDto } from 'src/common/dto/page.dto';
+import SysConfig from 'src/entities/admin/sys-config.entity';
+import { SysParamConfigService } from './param-config.service';
 
 @ApiTags('参数配置模块')
 @Controller('param-config')
-export class SysParamConfigController {}
+export class SysParamConfigController {
+  constructor(private paramConfigService: SysParamConfigService) {}
+
+  @ApiOperation({ summary: '查询当前在线用户' })
+  @ApiOkResponse({ type: [SysConfig] })
+  @Get('page')
+  async page(@Query() dto: PageOptionsDto): Promise<PageResult<SysConfig>> {
+    const list = await this.paramConfigService.getConfigListByPage(
+      dto.page - 1,
+      dto.limit,
+    );
+    const count = await this.paramConfigService.countConfigList();
+    return {
+      pagination: {
+        total: count,
+        size: dto.limit,
+        page: dto.page,
+      },
+      list,
+    };
+  }
+}
--- src/modules/admin/system/param-config/param-config.service.ts
+++ src/modules/admin/system/param-config/param-config.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import SysConfig from 'src/entities/admin/sys-config.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class SysParamConfigService {}
+export class SysParamConfigService {
+  constructor(
+    @InjectRepository(SysConfig)
+    private configRepository: Repository<SysConfig>,
+  ) {}
+
+  /**
+   * 罗列所有配置
+   */
+  async getConfigListByPage(page: number, count: number): Promise<SysConfig[]> {
+    return this.configRepository.find({
+      order: {
+        id: 'ASC',
+      },
+      take: count,
+      skip: page * count,
+    });
+  }
+
+  /**
+   * 获取参数总数
+   */
+  async countConfigList(): Promise<number> {
+    return this.configRepository.count();
+  }
+}
