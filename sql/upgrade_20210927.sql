f35de609cbb137b0f8e05c08f83f3775370e8d32
hackycy <qa894178522@qq.com>
2021-09-27 17:07:34 +0800

    chore: add table sql ddl

---
+++ sql/upgrade_20210927.sql
@@ -0,0 +1,14 @@
+-- `sf-admin`.sys_config definition
+
+DROP TABLE IF EXISTS `sys_config`;
+CREATE TABLE `sys_config` (
+  `created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_2c363c25cf99bcaab3a7f389ba` (`key`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
