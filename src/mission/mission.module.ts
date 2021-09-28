69e1a844e59a830fddb64fc7dbd012ad5ec3b01d
hackycy <qa894178522@qq.com>
2021-09-28 15:18:25 +0800

    refactor: change mission module register method

--- src/mission/mission.module.ts
+++ src/mission/mission.module.ts
@@ -1,4 +1,4 @@
-import { ExistingProvider, Global, Module } from '@nestjs/common';
+import { DynamicModule, ExistingProvider, Module } from '@nestjs/common';
 import { AdminModule } from 'src/modules/admin/admin.module';
 import { SysLogService } from 'src/modules/admin/system/log/log.service';
 import { SysLogClearJob } from './jobs/sys-log-clear.job';
@@ -23,16 +23,20 @@ function createAliasProviders(): ExistingProvider[] {
   return aliasProviders;
 }
 
-// 使用Alias定义别名，使得可以通过字符串类型获取定义的Service，否则无法获取
-const aliasProviders = createAliasProviders();
-
 /**
  * 所有需要执行的定时任务都需要在这里注册
  */
-@Global()
-@Module({
-  imports: [AdminModule],
-  providers: [...providers, ...aliasProviders, SysLogService],
-  exports: aliasProviders,
-})
-export class MissionModule {}
+@Module({})
+export class MissionModule {
+  static forRoot(): DynamicModule {
+    // 使用Alias定义别名，使得可以通过字符串类型获取定义的Service，否则无法获取
+    const aliasProviders = createAliasProviders();
+    return {
+      global: true,
+      module: MissionModule,
+      imports: [AdminModule],
+      providers: [...providers, ...aliasProviders, SysLogService],
+      exports: aliasProviders,
+    };
+  }
+}
