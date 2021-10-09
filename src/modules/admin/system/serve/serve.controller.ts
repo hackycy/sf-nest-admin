a3850304fcac9c3958d76f458f4c3937b8ef7ae7
hackycy <qa894178522@qq.com>
2021-10-09 17:37:14 +0800

    feat: impl serve stat api

--- src/modules/admin/system/serve/serve.controller.ts
+++ src/modules/admin/system/serve/serve.controller.ts
@@ -6,6 +6,7 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { ADMIN_PREFIX } from '../../admin.constants';
+import { PermissionOptional } from '../../core/decorators/permission-optional.decorator';
 import { ServeStatInfo } from './serve.class';
 import { SysServeService } from './serve.service';
 
@@ -17,8 +18,9 @@ export class SysServeController {
 
   @ApiOperation({ summary: '获取服务器运行信息' })
   @ApiOkResponse({ type: ServeStatInfo })
+  @PermissionOptional()
   @Get('stat')
   async stat(): Promise<ServeStatInfo> {
-    return {};
+    return await this.serveService.getServeStat();
   }
 }
